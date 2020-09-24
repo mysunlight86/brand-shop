dc99cd10afcff2e185410d88ccbc4129f1b19d7b
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-09-24 15:12:23 +0300

    Added a block for dynamic product insertion to the layout. And I created an array of products from which the product catalog will be created.

---
+++ js/catalog.js
@@ -0,0 +1,49 @@
+const catalogId = document.getElementById('catalog');
+
+const product = [
+  {
+    url: 'img/onproduct-1.jpg',
+    name: 'Mango People T-shirt',
+    price: '100',
+  },
+  {
+    url: 'img/onproduct-2.jpg',
+    name: 'Mango People T-shirt',
+    price: '100',
+  },
+  {
+    url: 'img/onproduct-3.jpg',
+    name: 'Mango People T-shirt',
+    price: '100',
+  },
+  {
+    url: 'img/onproduct-4.jpg',
+    name: 'Mango People T-shirt',
+    price: '100',
+  },
+  {
+    url: 'img/onproduct-5.jpg',
+    name: 'Mango People T-shirt',
+    price: '100',
+  },
+  {
+    url: 'img/onproduct-6.jpg',
+    name: 'Mango People T-shirt',
+    price: '100',
+  },
+  {
+    url: 'img/onproduct-7.jpg',
+    name: 'Mango People T-shirt',
+    price: '100',
+  },
+  {
+    url: 'img/onproduct-8.jpg',
+    name: 'Mango People T-shirt',
+    price: '100',
+  },
+  {
+    url: 'img/onproduct-9.jpg',
+    name: 'Mango People T-shirt',
+    price: '100',
+  },
+];
